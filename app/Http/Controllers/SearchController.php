4274fd197f7cbef76337aa05f6bb46a6edba7db6
Md Habib Al Muttakii Khan <m04khan1995@gmail.com>
2022-02-21 21:52:05 +0600

    search issue
    all variable added

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,11 +9,19 @@ use App\staffs;
 use Illuminate\Support\Facades\Route; 
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use App\contacts;
+use App\item;
+use App\about;
+use App\category;
+use App\exhibition;
 
 class SearchController extends Controller
 {
    public function searchitem(Request $request){
         $query = $request->get('search');
+        $contacts = contacts::where('id',1)->first();
+        
+
          $items = DB::table('items')         
                   ->join('categories','categories.id','=','categoryId')
                   ->select('items.*','categories.categoryName as catName')       
@@ -26,21 +34,27 @@ class SearchController extends Controller
 
                   if(Auth::user()==null){
                     $user = User::where('id',1)->first();
-            
+                    $textcolor = $user->textcolor;
                     $theme = $user->theme;
                     $logo = $user->logo;
                     $font = $user->font;
-                    return view('frontView.home.item', compact('items','theme','logo','font'));
+                    $categories = category::all();
+                    $footimg = $user->footimg;
+
+                    return view('frontView.home.item', compact('items','categories','footimg','textcolor','user','contacts','theme','logo','font'));
             
                   }else{
             
                     $user = User::where('id',Auth::id())->first();
                     // $user = User::where('id',1)->first();
+                    $textcolor = $user->textcolor;
                     $theme = $user->theme;
                     $logo = $user->logo;
                     $font = $user->font;
-              
-                    return view('frontView.home.item', compact('items','theme','logo','font'));
+                    $categories = category::all();
+                    $footimg = $user->footimg;
+
+                    return view('frontView.home.item', compact('items','textcolor','categories','footimg','user','theme','logo','font'));
                   }
         // return view('frontView.home.item',['items'=>$items]);
     }
